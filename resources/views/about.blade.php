57a5f713f15d5591c4d8e919b19e7c35a5d34fb5
Yoga Bagja Ramadhan <zombiexshine@gmail.com>
2022-10-28 11:35:03 +0700

    menyelesaikan video 4

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,19 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.main')
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>YBR Blog | About</title>
-</head>
-
-<body>
+@section('container')
     <h1>Halaman About</h1>
-    <h3><?= $name; ?></h3>
-    <p><?= $email; ?></p>
-    <img src="img/<?= $image; ?>" alt="<?= $name; ?>" width="200px">
-</body>
-
-</html>
+    <h3>{{ $name }}</h3>
+    <p>{{ $email }}</p>
+    <img src="img/{{ $image }}" alt="{{ $name }}" width="200px">
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,70 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Yoga Bagja Ramadhan",
         "email" => "203040112@mail.unpas.ac.id",
         "image" => "yoga.png"
     ]);
 });
 
+
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Yoga Bagja Ramadhan",
+            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex, reprehenderit repudiandae. Iste nisi dolore omnis veniam doloremque saepe, sint quis, laboriosam aliquid temporibus impedit amet natus optio numquam, qui ex cum sit! Rem voluptate maiores quae tempore omnis quidem, ipsa adipisci odit fugit voluptatem, dolorem cupiditate. Quo impedit neque necessitatibus eum ratione, reprehenderit consequuntur odio nemo cupiditate nobis suscipit perspiciatis sunt est obcaecati distinctio nisi quae facilis. Repellat animi nemo, error voluptatibus mollitia porro praesentium non commodi molestiae repellendus aspernatur."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Asril Permana",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto dignissimos, culpa totam laborum, obcaecati dicta voluptatum sed, praesentium fugiat reiciendis excepturi! Totam eveniet odit, eius blanditiis odio placeat! Earum ipsam facilis nulla iusto amet quia explicabo delectus veritatis tenetur ea eaque ex officia quibusdam porro suscipit laborum tempora cumque unde magnam animi a reprehenderit ipsum, quod repellendus. Maiores hic dolorum nesciunt sunt tempora maxime molestias, nostrum ratione consequuntur nulla corporis rerum iure possimus nihil accusantium doloremque porro, impedit esse quaerat quibusdam rem. Necessitatibus modi distinctio eveniet perferendis, libero voluptatum, ipsam doloremque excepturi commodi nam pariatur eligendi dolore blanditiis quo beatae?"
+        ],
+    ];
+
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+
+// halaman single post
+Route::get('posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Yoga Bagja Ramadhan",
+            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex, reprehenderit repudiandae. Iste nisi dolore omnis veniam doloremque saepe, sint quis, laboriosam aliquid temporibus impedit amet natus optio numquam, qui ex cum sit! Rem voluptate maiores quae tempore omnis quidem, ipsa adipisci odit fugit voluptatem, dolorem cupiditate. Quo impedit neque necessitatibus eum ratione, reprehenderit consequuntur odio nemo cupiditate nobis suscipit perspiciatis sunt est obcaecati distinctio nisi quae facilis. Repellat animi nemo, error voluptatibus mollitia porro praesentium non commodi molestiae repellendus aspernatur."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Asril Permana",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto dignissimos, culpa totam laborum, obcaecati dicta voluptatum sed, praesentium fugiat reiciendis excepturi! Totam eveniet odit, eius blanditiis odio placeat! Earum ipsam facilis nulla iusto amet quia explicabo delectus veritatis tenetur ea eaque ex officia quibusdam porro suscipit laborum tempora cumque unde magnam animi a reprehenderit ipsum, quod repellendus. Maiores hic dolorum nesciunt sunt tempora maxime molestias, nostrum ratione consequuntur nulla corporis rerum iure possimus nihil accusantium doloremque porro, impedit esse quaerat quibusdam rem. Necessitatibus modi distinctio eveniet perferendis, libero voluptatum, ipsam doloremque excepturi commodi nam pariatur eligendi dolore blanditiis quo beatae?"
+        ],
+    ];
+
+    $new_post = [];
+    foreach ($blog_posts as $post) {
+        if ($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
