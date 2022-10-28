6ce1d2e86a2176dbec91c1f0a47aa747b41027be
Yoga Bagja Ramadhan <zombiexshine@gmail.com>
2022-10-28 10:22:58 +0700

    menyelesaikan video 3

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Yoga Bagja Ramadhan",
+        "email" => "203040112@mail.unpas.ac.id",
+        "image" => "yoga.png"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
