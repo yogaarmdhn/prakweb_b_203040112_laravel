62175301ee0e053d56f9fdb522a2b3bb2f566ca3
Yoga Bagja Ramadhan <zombiexshine@gmail.com>
2022-10-29 07:01:43 +0700

    menyelesaikan video 9

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +23,63 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name' => 'Yoga Bagja Ramadhan',
+            'email' => '203040112@mail.unpas.ac.id',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Asril Permana',
+            'email' => '203040118@mail.unpas.ac.id',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, asperiores dolorem? Officia esse soluta qui nihil porro debitis eos',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, asperiores dolorem? Officia esse soluta qui nihil porro debitis eos quaerat fugit facere blanditiis, optio veniam necessitatibus iste quam. Cumque minima assumenda dignissimos a architecto doloremque quo fuga eos maiores vero, quae neque ut veritatis ad. Tempore iste recusandae beatae et, sequi hic, ea aliquid provident consequuntur delectus porro deleniti, enim explicabo cum doloremque repudiandae debitis tempora nisi ad esse pariatur similique aperiam nostrum. Tenetur obcaecati tempora eaque autem et saepe, quod a, dolorum fuga officiis similique esse rerum, labore ipsa iusto veniam! Officia esse nulla cumque. Excepturi fugit vitae ea necessitatibus sapiente optio quo debitis, est autem similique sit enim, totam consequatur adipisci nam. Autem, sequi suscipit facere tempore dolore numquam distinctio consequuntur odio perferendis neque id ab minus eius mollitia corporis voluptates veritatis sapiente magnam unde reiciendis eligendi doloribus consequatur. Iure sed id iste provident doloremque accusantium quae deleniti.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Dua',
+            'slug' => 'judul-ke-dua',
+            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, asperiores dolorem? Officia esse soluta qui nihil porro debitis eos',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, asperiores dolorem? Officia esse soluta qui nihil porro debitis eos quaerat fugit facere blanditiis, optio veniam necessitatibus iste quam. Cumque minima assumenda dignissimos a architecto doloremque quo fuga eos maiores vero, quae neque ut veritatis ad. Tempore iste recusandae beatae et, sequi hic, ea aliquid provident consequuntur delectus porro deleniti, enim explicabo cum doloremque repudiandae debitis tempora nisi ad esse pariatur similique aperiam nostrum. Tenetur obcaecati tempora eaque autem et saepe, quod a, dolorum fuga officiis similique esse rerum, labore ipsa iusto veniam! Officia esse nulla cumque. Excepturi fugit vitae ea necessitatibus sapiente optio quo debitis, est autem similique sit enim, totam consequatur adipisci nam. Autem, sequi suscipit facere tempore dolore numquam distinctio consequuntur odio perferendis neque id ab minus eius mollitia corporis voluptates veritatis sapiente magnam unde reiciendis eligendi doloribus consequatur. Iure sed id iste provident doloremque accusantium quae deleniti.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul ke Tiga',
+            'slug' => 'judul-ke-tiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, asperiores dolorem? Officia esse soluta qui nihil porro debitis eos',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, asperiores dolorem? Officia esse soluta qui nihil porro debitis eos quaerat fugit facere blanditiis, optio veniam necessitatibus iste quam. Cumque minima assumenda dignissimos a architecto doloremque quo fuga eos maiores vero, quae neque ut veritatis ad. Tempore iste recusandae beatae et, sequi hic, ea aliquid provident consequuntur delectus porro deleniti, enim explicabo cum doloremque repudiandae debitis tempora nisi ad esse pariatur similique aperiam nostrum. Tenetur obcaecati tempora eaque autem et saepe, quod a, dolorum fuga officiis similique esse rerum, labore ipsa iusto veniam! Officia esse nulla cumque. Excepturi fugit vitae ea necessitatibus sapiente optio quo debitis, est autem similique sit enim, totam consequatur adipisci nam. Autem, sequi suscipit facere tempore dolore numquam distinctio consequuntur odio perferendis neque id ab minus eius mollitia corporis voluptates veritatis sapiente magnam unde reiciendis eligendi doloribus consequatur. Iure sed id iste provident doloremque accusantium quae deleniti.',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul ke Empat',
+            'slug' => 'judul-ke-empat',
+            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, asperiores dolorem? Officia esse soluta qui nihil porro debitis eos',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, asperiores dolorem? Officia esse soluta qui nihil porro debitis eos quaerat fugit facere blanditiis, optio veniam necessitatibus iste quam. Cumque minima assumenda dignissimos a architecto doloremque quo fuga eos maiores vero, quae neque ut veritatis ad. Tempore iste recusandae beatae et, sequi hic, ea aliquid provident consequuntur delectus porro deleniti, enim explicabo cum doloremque repudiandae debitis tempora nisi ad esse pariatur similique aperiam nostrum. Tenetur obcaecati tempora eaque autem et saepe, quod a, dolorum fuga officiis similique esse rerum, labore ipsa iusto veniam! Officia esse nulla cumque. Excepturi fugit vitae ea necessitatibus sapiente optio quo debitis, est autem similique sit enim, totam consequatur adipisci nam. Autem, sequi suscipit facere tempore dolore numquam distinctio consequuntur odio perferendis neque id ab minus eius mollitia corporis voluptates veritatis sapiente magnam unde reiciendis eligendi doloribus consequatur. Iure sed id iste provident doloremque accusantium quae deleniti.',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -5,7 +5,7 @@
 
     @foreach ($posts as $post)
         <article class="mb-5">
-            <h2><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h2>
+            <h2><a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a></h2>
             <p>{{ $post->excerpt }}</p>
         </article>
     @endforeach
