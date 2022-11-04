f253327fa2aa2dadd449ba1af9d3cf55f9480537
Yoga Bagja Ramadhan <zombiexshine@gmail.com>
2022-11-04 18:25:53 +0700

    menyelesaikan video 12

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,7 @@ Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::get('/categories', function () {
     return view('categories', [
         'title' => 'Post Categories',
+        'active' => 'categories',
         'categories' => Category::all()
     ]);
 });
@@ -46,6 +47,7 @@ Route::get('/categories', function () {
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'title' => "Post By Category : $category->name",
+        'active' => 'categories',
         'posts' => $category->posts->load('category', 'author')
     ]);
 });
